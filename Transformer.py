0e1f6f8787a57f2ffe8e90d7f9a37f190586369c
nicklin910221 <nicklin910221@gmail.com>
2023-10-05 15:17:25 +0000

    edit Transformer

--- Transformer.py
+++ Transformer.py
@@ -8,26 +8,39 @@ class Transformer(nn.Module):
         super(Transformer, self).__init__()
         self.attention_heads = attention_heads
         self.layer = layer
+        self.size = size
 
         self.device = device
 
         assert (size[0] % attention_heads == 0 and size[1] % attention_heads == 0), "attention_heads can't slice the image."
 
-        self.query = nn.Parameter(torch.randn(layer, size[0] / attention_heads, size[1] / attention_heads, attention_heads, attention_heads))
-        self.key = nn.Parameter(torch.randn(layer, size[0] / attention_heads, size[1] / attention_heads, attention_heads, attention_heads))
-        self.value = nn.Parameter(torch.randn(layer, size[0] / attention_heads, size[1] / attention_heads, attention_heads, attention_heads))
+        self.r = int(size[0] / attention_heads)
+        self.c = int(size[1] / attention_heads)
+
+        self.query = nn.Parameter(torch.randn(layer, attention_heads, attention_heads, self.r, self.c))
+        self.key = nn.Parameter(torch.randn(layer, attention_heads, attention_heads, self.r, self.c))
+        self.value = nn.Parameter(torch.randn(layer, attention_heads, attention_heads, self.r, self.c))
 
         self.dropout = [nn.Dropout(dropout) for l in range(layer)]
 
     def forward(self, x):
+        num = x.shape[0]
+        channel = x.shape[1]
+
         for l in range(self.layer):
+            heads = []
             for r in range(self.attention_heads):
                 for c in range(self.attention_heads):
-                    heads = []
-                    heads.append(torch.matmul(x, self.attention(self.query[l][r][c], self.key[l][r][c], self.value[l][r][c])))
-                    heads = torch.cat(heads, 1)
-            x += self.dropout[l](heads)
-        return x
+                    a = torch.matmul(x[:,:,self.r * r:self.r * (r + 1),self.c * c:self.c * (c + 1)], self.attention(self.query[l][r][c], self.key[l][r][c], self.value[l][r][c]))
+                    heads.append(a)
+            # heads = torch.cat(heads, 1)
+            out = torch.Tensor(num, channel, self.attention_heads ** 2, self.r, self.c)
+            torch.cat(heads, out = out)
+
+            # heads = heads.view(num, channel)
+            print(heads.shape)
+            # x += self.dropout[l](heads)
+        # return x
 
     def attention(self, q, k, v):
         score = torch.matmul(torch.matmul(q, k.permute(0, 1)), v)
