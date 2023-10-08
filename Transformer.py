eef5f301ae50bdd3657ccfd320b453f26b50089d
林宥叡 <dearestbee@DearestBee-MacBook-Air.local>
2023-10-08 16:16:42 +0800

    add MLP

--- Transformer.py
+++ Transformer.py
@@ -2,9 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features = None, out_features = None, activation_func = nn.LeakyReLU, dropout = 0) -> None:
+        super(MLP, self).__init__()
+
+        out_features = in_features or out_features
+        hidden_features = in_features or hidden_features
+        self.layer1 = nn.Linear(in_features, hidden_features)
+        self.activation_func = activation_func()
+        self.layer2 = nn.Linear(hidden_features, out_features)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.activation_func(x)
+        x = self.dropout(x)
+        x = self.layer2(x)
+        x = self.dropout(x)
+        return x
+
 class Transformer(nn.Module):
 
-    def __init__(self, device, layer = 3, attention_heads = 1, size = (28, 28), dropout = 0.1) -> None:
+    def __init__(self, device, layer = 3, attention_heads = 1, size = (28, 28), dropout = 0) -> None:
         super(Transformer, self).__init__()
         self.attention_heads = attention_heads
         self.layer = layer
@@ -21,23 +40,28 @@ class Transformer(nn.Module):
         self.key = nn.Parameter(torch.randn(layer, attention_heads, attention_heads, self.r, self.c))
         self.value = nn.Parameter(torch.randn(layer, attention_heads, attention_heads, self.r, self.c))
 
-        self.dropout = [nn.Dropout(dropout) for l in range(layer)]
+        self.MLP = [MLP(size[0] * size[1]) for l in range(layer)]
+        self.norm = nn.LayerNorm
 
     def forward(self, x):
         num = x.shape[0]
         channel = x.shape[1]
         for l in range(self.layer):
             heads = []
+            x1 = self.norm(x)
             for r in range(self.attention_heads):
                 for c in range(self.attention_heads):
-                    a = torch.matmul(x[:,:,self.r * r:self.r * (r + 1),self.c * c:self.c * (c + 1)], self.attention(self.query[l][r][c], self.key[l][r][c], self.value[l][r][c]))
-                    heads.append(a)
+                    heads.append(torch.matmul(x1[:,:,self.r * r:self.r * (r + 1),self.c * c:self.c * (c + 1)], self.attention(self.query[l][r][c], self.key[l][r][c], self.value[l][r][c])))
             for r in range(self.attention_heads):
                 for c in range(1, self.attention_heads):
                     heads[r * self.attention_heads] = torch.cat((heads[r * self.attention_heads], heads[r * self.attention_heads + c]), dim = 2)
                 if r > 0:
                     heads[0] = torch.cat((heads[0], heads[r * self.attention_heads]), dim = 3)
-        return heads[0]
+            x2 = x + heads[0]
+            x3 = self.norm(x2)
+            x4 = self.MLP[l](x)
+            x = x4 + x2
+        return x
 
     def attention(self, q, k, v):
         score = torch.matmul(q, k.permute(0, 1))
